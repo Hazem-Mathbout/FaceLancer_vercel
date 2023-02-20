ded5f0aac3a97d3b1582a48c06950774140e2ca8
Hazem-Mathbout <hazemmathbout93@gmail.com>
2023-02-20 06:00:24 +0200

    sexth commit

--- api/app.py
+++ api/app.py
@@ -18,6 +18,8 @@ def index():
     # A welcome message to test our server
     return "<h1>Welcome to our medium-greeting-api!</h1>"
 
+# ------------------------ Start khamsat scrapping---------------------------
+
 
 @app.route("/resKham", methods=["POST", "GET"])
 def scrapKhamsat(requests_session=None, output=None):
@@ -152,6 +154,227 @@ def taskScrapLinksKhamsat(offer, requests_session):
         return offer
     except Exception as exc:
         print(f"This Exception From mostaql get offer the error is : {exc}")
+
+# ------------------------ End khamsat scrapping---------------------------
+
+# ------------------------ Start Mostaql scrapping---------------------------
+
+
+@app.route("/resMost", methods=["POST", "GET"])
+def scrapmostaql(requests_session=None, output=None):
+    isFuncInternal = False
+    if output == None:
+        requests_session = requests.Session()
+        try:
+            output = request.get_json()
+        except Exception as exc:
+            pass
+            print(
+                f"generated an exception when convert to json in route /resMost =>: {exc}")
+            print(f"The output Now in /resMost is: {output}")
+    else:
+        isFuncInternal = True
+    offset = output["offset_mostaql"]
+    limit = 25 if output["limit"] > 25 else output["limit"]
+    budget_max = 10000 if output["budget_max"] == "None" else output["budget_max"]
+    budget_min = 0.00 if output["budget_min"] == "None" else output["budget_min"]
+    num_bage_mostaql = 1 if output["num_bage_mostaql"] == "None" or 0 else output["num_bage_mostaql"]
+    category_mostaql = output["category_mostaql"]
+    delivery_duration_for_mostaql = "" if output[
+        "delivery_duration_for_mostaql"] == "None" else output["delivery_duration_for_mostaql"]
+    skills_for_mostaql = output["skills_for_mostaql"]
+    searchTerm = output["searchTerm"]
+
+    # finalRes = {}
+    listResult = []
+
+    if category_mostaql == "None":
+        URL = f"https://mostaql.com/projects?page={num_bage_mostaql}&keyword={searchTerm}&skills={skills_for_mostaql}&duration={delivery_duration_for_mostaql.removesuffix(',')}&budget_min={budget_min}&budget_max={budget_max}&sort=latest"
+    else:
+        URL = f"https://mostaql.com/projects?page={num_bage_mostaql}&keyword={searchTerm}&category={category_mostaql}&skills={skills_for_mostaql}&duration={delivery_duration_for_mostaql.removesuffix(',')}&budget_min={budget_min}&budget_max={budget_max}&sort=latest"
+    try:
+        sourcPage = requests_session.get(URL, headers=HEADERS)
+        sourcSoup = BeautifulSoup(sourcPage.text, "lxml")
+        tempRes = sourcSoup.findAll(name='tr', attrs={"class": "project-row"})
+        tempRes = tempRes[offset: offset+limit]
+        print(f"temp result length in mostaql is: {len(tempRes)}")
+        if (len(tempRes) != 0):
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                future_to_offer = {executor.submit(
+                    taskScrapMostaql, offer, requests_session): offer for offer in tempRes}
+                for future in concurrent.futures.as_completed(future_to_offer):
+                    offer = future_to_offer[future]
+                    try:
+                        data = future.result()
+                    except Exception as exc:
+                        print('%r generated an exception: %s' % (offer, exc))
+                    else:
+                        print("***************************************************")
+                        if len(data) != 0:
+                            print(data)
+                            listResult.append(data)
+                    #  listResult.append({"postId" : postId , "dateTime" : dateTime , "publisher" : publisher , "statusOfPublisher" : None ,  "webSiteName" : "mostaql" , "title" : title , "content" : content , "url" : url , "time" : time , "status" : status , "price" : price , "number_of_offers" : number_of_offers , "url_img" : url_img})
+        print(f'Number Offer mostaql is: {len(listResult)}')
+    except Exception as exc:
+        print(f"This Exception When Connect to Mostaql the error is : {exc}")
+    try:
+        listResult.sort(key=lambda x: x['dateTime'], reverse=True)
+    except Exception as exc:
+        print(f"Error occure when sorting offers mostaql, error is:{exc}")
+    if isFuncInternal:
+        finalRes = json.dumps(listResult)
+        return (finalRes)
+    else:
+        requests_session.close()
+        return jsonify(listResult)
+
+
+def taskScrapMostaql(res, requests_session) -> dict:
+    myDict = {}
+    try:
+        title = res.find('a').text
+        url = res.find('a').get_attribute_list('href')[0]
+        time = res.find('time').text.strip()
+        time = "".join(time.split())
+        number_of_offers = res.find('ul').findAll('li')[2].text.strip()
+        postId = url.split('-')[0].split('/')[-1]
+        ########################################################
+        webpage2 = requests_session.get(url, headers=HEADERS)
+        soup = BeautifulSoup(webpage2.text, "lxml")
+        content = soup.find(name='div', attrs={
+                            "class": "text-wrapper-div carda__content"}).text
+        content = " ".join(content.split())
+        publisher = soup.find(name='h5', attrs={
+                              "class": "postcard__title profile__name mrg--an"}).find(name='bdi').text
+        status = soup.find(name='bdi', attrs={
+                           "class": "label label-prj-open"}).text
+        price = soup.find(name='span', attrs={"dir": "rtl"}).text
+        url_img = soup.find(name='div', attrs={
+                            "class": "profile-card--avatar dsp--f small_avatar_container"}).find('img').get_attribute_list('src')[0]
+        dateTime = soup.find(name='td', attrs={
+                             "data-type": "project-date"}).find(name='time').get_attribute_list('datetime')[0]
+        ########################################################
+        myDict = {"postId": postId, "dateTime": dateTime, "publisher": publisher, "statusOfPublisher": None,  "webSiteName": "mostaql", "title": title,
+                  "content": content, "url": url, "time": time, "status": status, "price": price, "number_of_offers": number_of_offers, "url_img": url_img}
+    except Exception as exc:
+        print(f"This Exception From mostaql get offer the error is : {exc}")
+    return myDict
+
+# ------------------------ End Mostaql scrapping---------------------------
+
+# ------------------------ Start Kafiil scrapping---------------------------
+
+
+@app.route("/resKafi", methods=["POST", "GET"])
+def scrapkafiil(requests_session=None, output=None):
+    isFuncInternal = False
+    if output == None:
+        requests_session = requests.Session()
+        try:
+            # output = json.loads(request.data, strict = False)
+            output = request.get_json()
+        except Exception as exc:
+            pass
+            print(
+                f"generated an exception when convert to json in route /resKafi => : {exc}")
+    else:
+        isFuncInternal = True
+    num_bage_kafiil = 1 if output["num_bage_kafiil"] == "None" or 0 else output["num_bage_kafiil"]
+    category_kafiil = output["category_kafiil"]
+    delivery_duration_for_kafiil = "" if output[
+        "delivery_duration_for_kafiil"] == "None" else output["delivery_duration_for_kafiil"]
+    budget_max = 10000 if output["budget_max"] == "None" else output["budget_max"]
+    budget_min = 0.00 if output["budget_min"] == "None" else output["budget_min"]
+    searchTerm = output["searchTerm"]
+    offset = output["offset_kafiil"]
+    limit = 25 if output["limit"] > 25 else output["limit"]
+
+    listResult = []
+
+    if category_kafiil == "None":
+        URL = f"https://kafiil.com/kafiil/public/projects?delivery_duration={delivery_duration_for_kafiil.removesuffix(',')}&page={num_bage_kafiil}&search={searchTerm}&source=web"
+    else:
+        URL = f"https://kafiil.com/kafiil/public/projects/{category_kafiil}?delivery_duration={delivery_duration_for_kafiil.removesuffix(',')}&page={num_bage_kafiil}&search={searchTerm}&source=web"
+    try:
+        sourcPage = requests_session.get(URL, headers=HEADERS, )
+        sourcSoup = BeautifulSoup(sourcPage.text, "lxml")
+        tempRes = sourcSoup.findAll(
+            name='div', attrs={"class": "project-box active"})
+        tempRes = tempRes[offset: offset+limit]
+        if len(tempRes) != 0:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                future_to_offer = {executor.submit(
+                    taskScrapKafiil, offer, requests_session, budget_min, budget_max): offer for offer in tempRes}
+                for future in concurrent.futures.as_completed(future_to_offer):
+                    offer = future_to_offer[future]
+                    try:
+                        data = future.result()
+                    except Exception as exc:
+                        print('%r generated an exception: %s' % (offer, exc))
+                    else:
+                        print("***************************************************")
+                        if len(data) != 0:
+                            print(data)
+                            listResult.append(data)
+        print(f"Number offers in kafiil {len(listResult)}")
+
+    except Exception as exc:
+        print(f"This Exception When connect To Kafiil the error is : {exc}")
+    try:
+        listResult.sort(key=lambda x: x['dateTime'], reverse=True)
+    except Exception as axc:
+        print(f"Error occure when sorting offers kafiil, error is:{exc}")
+    if isFuncInternal:
+        finalRes = json.dumps(listResult)
+        return (finalRes)
+    else:
+        requests_session.close()
+        return jsonify(listResult)
+
+
+def taskScrapKafiil(res, requests_session, budget_min, budget_max) -> dict:
+    myDict = {}
+    price = res.findAll('p')[0].text.strip()
+    list_price = price.split('-')
+    numMin = int(list_price[0].strip().removeprefix('$'))
+    numMax = int(list_price[1].strip().removeprefix('$'))
+    budget_max = int(float(budget_max))
+    budget_min = int(float(budget_min))
+    if ((budget_min > 0 or budget_max < 10000) and not (numMin >= budget_min and numMax <= budget_max)):
+        return
+    try:
+        title = res.findAll('a')[1].text.split()
+        if (title[0] != "قيد"):
+            title = " ".join(title[1:])
+        else:
+            title = " ".join(title[2:])
+        url = res.findAll('a')[1].get_attribute_list('href')[0]
+        time = res.findAll('span')[1].text.strip()
+        status = res.findAll('span')[0].text
+        number_of_offers = res.findAll('span')[2].text.strip()
+        url_img = res.find('img').get_attribute_list('src')[0]
+        postId = url.split('-')[0].split('/')[-1]
+        #################################################
+        webpage2 = requests_session.get(url, headers=HEADERS)
+        soup = BeautifulSoup(webpage2.text, "lxml")
+        content = soup.find(name='p', attrs={"class": ""}).text
+        content = " ".join(content.split())
+        publisher = soup.find(
+            name='div', attrs={"class": "user-info-row"}).find('div').find('a').text
+        publisher = " ".join(publisher.split())
+        dateTime = soup.find(name='span', attrs={
+                             "data-toggle": "tooltip"}).get_attribute_list('title')[0]
+        myDict = {"postId": postId, "dateTime": dateTime, "publisher": publisher, "statusOfPublisher": None,  "webSiteName": "kafiil", "title": title,
+                  "content": content, "url": url, "time": time, "status": status, "price": price, "number_of_offers": number_of_offers, "url_img": url_img}
+    except Exception as exc:
+        print(f"This Exception From kafiil get offer the error is : {exc}")
+    return myDict
+
+# ------------------------ End Kafiil scrapping---------------------------
+
+# ------------------------ Start Notification scrapping-------------------
+
+# ------------------------ End Notification scrapping---------------------------
 
 
 if __name__ == '__main__':
